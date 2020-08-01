63b210c275d4466e2b8429eadb639c7f9152db09
Charles Loges IV <c4webdev@gmail.com>
2020-07-31 21:06:34 -0400

    added javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,92 @@
 import '../scss/app.scss';
+
+// DOM element
+
+const passwordElement = document.getElementById('password');
+const lengthElement = document.getElementById('slider-length');
+const uppercaseElement = document.getElementById('uppercase');
+const lowercaseElement = document.getElementById('lowercase');
+const numbersElement = document.getElementById('numbers');
+const symbolsElement = document.getElementById('symbols');
+const generateElement = document.getElementById('generate');
+const clipboardElement = document.getElementById('clipboard');
+
+// Generate event listener
+
+generateElement.addEventListener('click', () => {
+  const length = +lengthElement.value;
+  const hasLower = lowercaseElement.checked;
+  const hasUpper = uppercaseElement.checked;
+  const hasNumber = numbersElement.checked;
+  const hasSymbol = symbolsElement.checked;
+
+  passwordElement.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// copy to clipboard
+
+clipboardElement.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = passwordElement.innerText;
+
+  if (!password) {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Password copied to clickboard.');
+});
+
+// Generate password
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+  const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter((item) => Object.values(item)[0]);
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArray.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
+// function generators
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}<>/?';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
